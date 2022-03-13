4620c1ad1f0f8b8a82fc9f95986867a53e6f18d9
Wellington Okabayashi <100711519+WellingtonOkabayashi@users.noreply.github.com>
2022-03-13 23:47:53 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const menu = document.querySelector('.menu')
 menu.onclick = function () {
   navigation.classList.toggle('active'), container.classList.toggle('active')
 }
+
+/*======---Menu ativo---=======*/
+const li = document.querySelectorAll('.link')
+const sec = document.querySelectorAll('section')
+
+function menuAtivo() {
+  let len = sec.length
+  while (--len && window.scrollY + 100 < sec[len].offsetTop) {}
+  li.forEach(ltx => ltx.classList.remove('ativo'))
+  li[len].classList.add('ativo')
+}
+menuAtivo()
+window.addEventListener('scroll', menuAtivo)
